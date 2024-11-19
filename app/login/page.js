aea1b14b45d39fa082f6e6a91b5a3526e31c8c9d
Mokesh-C <cmokesh17@gmail.com>
2024-11-19 20:52:55 +0530

    Created Navbar SignUp Login page

---
+++ app/login/page.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import '../styles/signup.login.css';
+
+export default function SignIn() {
+    return (
+        <form action="" className="sign-in">
+            <h1>Login</h1>
+            <div className="group-input">
+                <input type="email" id="email" name="email" placeholder="" />
+                <label htmlFor="email">Organization Email</label>
+                <div className="error"></div>
+            </div>
+            <div className="group-input">
+                <input type="password" id="password" name="password" placeholder="" />
+                <label htmlFor="password">Password</label>
+                <div className="error"></div>
+            </div>
+            <div className="remember" >
+                <label hhtmlFor="remember">
+                    <input type="checkbox" id="remember" />
+                    <p>Remember me</p>
+                </label>
+                <a href="">Forgot password</a>
+            </div>
+            <button type="submit">Log In</button>
+            <div className="account" >
+                <p>Don't have an account? <Link href={"/signup"}> Sign Up</Link></p>
+            </div >
+        </form >
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,20 @@
+
+import Image from "next/image";
+import './styles/home.css';
+export default function Home() {
+    return (
+        <main>
+            <div>
+                <Image className="img"
+                    src="/bg.jpg"
+                    alt="Sample Image"
+                    width={612}
+                    height={335}
+                    layout="responsive"
+                    style={{ maxWidth: '100%', maxHeight: '80.2vh' }}
+                />
+                <div className="hero">" Let’s Make Your Next Event <span className="hero-span">Extraordinary!</span> "</div>
+            </div>
+        </main >
+    );
+}
