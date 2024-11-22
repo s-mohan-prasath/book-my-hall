71079761aedfa106d632a330778ed6da2fd2dbd7
24mx212 <24mx212@MCA.PSG>
2024-11-22 11:56:44 +0530

    footer styled

--- frontend/app/components/Footer.js
+++ frontend/app/components/Footer.js
@@ -1,27 +1,41 @@
-import Link from "next/link";
-import "../styles/Footer.css";
+import Link from "next/link"
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutubeSquare } from "react-icons/fa";
+import "../styles/Footer.css"
 
-export default function () {
+export default function Footer() {
   return (
-    <div>{/* outer */}
-      <div>{/* inner */}
-        <div>{/* left */}
-          <h1>Book My Hall</h1>
-          <p>A Cloud-based Workplace Management Software</p>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h1 className="footer-title">Book My Hall</h1>
+          <p className="footer-description">A Cloud-based Workplace Management Software</p>
+          <div className="footer-social">
+            <a href="#" aria-label="LinkedIn">
+                <FaLinkedin size={24} />
+            </a>
+            <a href="#" aria-label="Instagram">
+              <FaInstagram size={24} />
+            </a>
+            <a href="#" aria-label="YouTube">
+              <FaYoutubeSquare size={24} />
+            </a>
+          </div>
         </div>
-        <div>{/* right */}
-          <h6>sitemap</h6>
-          <ul>
+        <div className="footer-right">
+          <h6 className="footer-sitemap-title">Sitemap</h6>
+          <ul className="footer-sitemap-list">
             <li><Link href="/">Home</Link></li>
-            <li><Link href="/venues">venues</Link></li>
+            <li><Link href="/venues">Venues</Link></li>
             <li><Link href="/signup">SignUp</Link></li>
             <li><Link href="/login">LogIn</Link></li>
           </ul>
         </div>
       </div>
-      <div>{/* down border */}
-        <p>&copy 2024 Book My Hall. All rights reserved.</p>
+      <div className="footer-bottom">
+        <p className="footer-copyright">&copy; 2024 Book My Hall. All rights reserved.</p>
       </div>
-    </div>
-  );
+    </footer>
+  )
 }
