335bc63a8fbe3efeb13c568ead08b1b81daca741
Mohan Prasath S <s.mohanprasath@gmail.com>
2024-11-25 21:45:06 +0530

    local commit

--- backend/api/admin/auth/index.js
+++ backend/api/admin/auth/index.js
@@ -21,7 +21,7 @@ Router.post("/signin", async (req, res) => {
         const token = await user.generateJwtToken();
         res.status(200).json({ token, status: "success" });
     } catch (error) {
-        return res.status(404).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 });
 
