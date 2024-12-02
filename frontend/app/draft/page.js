cf564e30403b6334a61affbc1dddfdc32f5b0bd9
Mohan Prasath S <s.mohanprasath@gmail.com>
2024-12-02 21:53:34 +0530

    work on getdeviceinfo function

--- frontend/app/draft/page.js
+++ frontend/app/draft/page.js
@@ -4,8 +4,16 @@ import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import dayjs from "dayjs"
 import { useCallback, useState } from "react"
 
+import { getDeviceInfo } from "../utils/navigator.functions"
 const DnDCalendar = withDragAndDrop(Calendar)
 const localizer = dayjsLocalizer(dayjs)
+
+/**
+ * 
+
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36
+
+ */
 
 export default function draft() {
     const [events, setEvents] = useState([
@@ -28,18 +36,22 @@ export default function draft() {
         })
     }, [events])
     return (
-        <div style={{ marginBottom: "500px", padding: "0 40px" }}>
-            <h1 style={{ fontWeight: "bold", fontSize: "40px", padding: "10px" }}>Event Calendar Page</h1>
-            <DnDCalendar
-                localizer={localizer}
-                events={events}
-                startAccessor={"start"}
-                endAccessor={"end"}
-                draggableAccessor={(event) => true}
-                style={{ height: "80vh" }}
-                onEventResize={handleEventPropChange}
-                onEventDrop={handleEventPropChange}
-            />
-        </div>
+        <main>
+            <button className="bg-black text-white p-3 m-5" onClick={getDeviceInfo}>CLICK</button>
+            <div style={{ marginBottom: "500px", padding: "0 40px" }}>
+                <h1 style={{ fontWeight: "bold", fontSize: "40px", padding: "10px" }}>Event Calendar Page</h1>
+                <DnDCalendar
+                    localizer={localizer}
+                    events={events}
+                    startAccessor={"start"}
+                    endAccessor={"end"}
+                    draggableAccessor={(event) => true}
+                    style={{ height: "80vh" }}
+                    onEventResize={handleEventPropChange}
+                    onEventDrop={handleEventPropChange}
+                />
+            </div>
+        </main>
+
     )
 }
